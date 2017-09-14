3a959c8b55fae8ba0abbc8ec30ffe2d692ee092a
Brandon Little <brlittle86@gmail.com>
2017-09-14 09:40:51 -0700

    built main menu with try/catch

--- lab03-brandon/WordGuessGame/WordGuessGame/Program.cs
+++ lab03-brandon/WordGuessGame/WordGuessGame/Program.cs
@@ -6,12 +6,45 @@ namespace WordGuessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainMenu();
         }
 
         static void MainMenu()
         {
+            Console.Clear();
+            Console.WriteLine("Please enter a number for your selection:");
+            Console.WriteLine("1) Game Menu");
+            Console.WriteLine("2) Read Word File");
+            Console.WriteLine("3) Add/Remove Words");
+            Console.WriteLine("0) Exit Game");
+            try
+            {
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        GameMenu();
+                        break;
+                    case 2:
+                        ReadWordFile();
+                        break;
+                    case 3:
+                        EditWordFile();
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid number");
+                        MainMenu();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.InnerException}: Exception has occurred. Press any key to return to Main Menu.");
+                Console.Read();
+                MainMenu();
 
+            }
         }
 
         static void GameMenu()
@@ -25,6 +58,11 @@ namespace WordGuessGame
         }
 
         static void ReadWordFile()
+        {
+
+        }
+
+        static void EditWordFile()
         {
 
         }
