eb218e8877c471867f7b191a22eefb68f4ad4680
Brandon Little <brlittle86@gmail.com>
2017-09-14 09:16:47 -0700

    framework for methods added

--- lab03-brandon/WordGuessGame/WordGuessGame/Program.cs
+++ lab03-brandon/WordGuessGame/WordGuessGame/Program.cs
@@ -8,5 +8,52 @@ namespace WordGuessGame
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void MainMenu()
+        {
+
+        }
+
+        static void GameMenu()
+        {
+
+        }
+
+        static void CreateWordFile()
+        {
+
+        }
+
+        static void ReadWordFile()
+        {
+
+        }
+
+        static void AddWordToFile()
+        {
+
+        }
+
+        static void RemoveWordFromFile()
+        {
+
+        }
+
+        static void StartNewGame()
+        {
+
+        }
+
+        static void ResumeGame()
+        {
+
+        }
+
+        static void SelectRandomWord()
+        {
+
+        }
+
+
     }
 }
