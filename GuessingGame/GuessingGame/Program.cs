fce978888077506af7a458eb220f406462a02eb7
Brian McFarlane <bmcfarlane82@gmail.com>
2017-09-14 20:15:47 -0700

    Creating Framework

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool CreateFile()
+        {
+
+            return true;
+        }
+
+        public static string[] ReadDictionary()
+        {
+            var filePath = @"C:\projects\401d1\Day-3-WordGuessGame\GuessingGame\GuessingGame\assets\words_alpha.txt";
+            if (!File.Exists(filePath))
+            {
+                using (StreamReader sr = File.OpenText(filePath))
+                {
+                    string[] words = File.ReadAllLines(filePath);
+                    return words;
+                }
+            }
+        }
+
+        public static bool UpdateFile()
+        {
+            return true;
+        }
+        public bool DeleteWord()
+        {
+            return true;
+        }
+
+        public static string ChooseWord()
+        {
+            return string;
+        }
+
+        public static string ViewDictionary()
+        {
+            return string;
+        }
+    }
+}
+
